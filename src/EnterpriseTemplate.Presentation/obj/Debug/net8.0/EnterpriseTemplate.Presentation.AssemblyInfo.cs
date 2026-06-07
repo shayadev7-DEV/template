7863b779af378a1fe2d78dc2a9f4c3f4f862bd92
younes.m <yones.m.g.oghli@gmail.com>
2026-06-07 13:09:46 +0330

    Commit

--- src/EnterpriseTemplate.Presentation/obj/Debug/net8.0/EnterpriseTemplate.Presentation.AssemblyInfo.cs
+++ src/EnterpriseTemplate.Presentation/obj/Debug/net8.0/EnterpriseTemplate.Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnterpriseTemplate.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee9bd13d42ae703bc37d4e82f570838f9517c275")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc08cea3274c4a7246c2c7b8b09274b84aace43a")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnterpriseTemplate.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnterpriseTemplate.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
