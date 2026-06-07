d1e1326eefbe77dbdb3dffd2aa4e22bfa0e6e5b7
younes.m <yones.m.g.oghli@gmail.com>
2026-06-07 11:45:31 +0330

    wwwroot added

--- src/EnterpriseTemplate.Presentation/obj/Debug/net8.0/EnterpriseTemplate.Presentation.AssemblyInfo.cs
+++ src/EnterpriseTemplate.Presentation/obj/Debug/net8.0/EnterpriseTemplate.Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnterpriseTemplate.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a9511d6f88a2a4647fc4d12bba2c148e0f40c4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b66aca1920a70951279d8d829aa63d4130047f2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnterpriseTemplate.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnterpriseTemplate.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
